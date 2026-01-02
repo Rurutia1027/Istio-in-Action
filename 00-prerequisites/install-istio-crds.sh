63844ad2950e4913ebe2094f1dceddd5e0844bf6
Rurutia1027 <rurutia1027@gmail.com>
2026-01-02 17:39:56 +0800

    modify shell script use istioctl to install crds

--- 00-prerequisites/install-istio-crds.sh
+++ 00-prerequisites/install-istio-crds.sh
@@ -2,25 +2,14 @@
 
 set -euo pipefail
 
-ISTIO_VERSION="1.28.2"
-ISTIO_REPO="https://raw.githubusercontent.com/istio/istio/${ISTIO_VERSION}" 
+ISTIO_VERSION="${ISTIO_VERSION:-1.28.2}"
 
 echo "Installing Istio CRDs (version: ${ISTIO_VERSION})"
 
-echo "Applying base CRDs..."
-kubectl apply -f "${ISTIO_REPO}/manifests/charts/base/crds"
+istioctl install \
+  --set profile=minimal \
+  --set components.pilot.enabled=false \
+  --skip-confirmation 
 
-echo "Waiting for CRDs to be established ..."
-kubectl wait --for=condition=Established crd \
-  virtualservices.networking.istio.io \
-  destinationrules.networking.istio.io \
-  gateways.networking.istio.io \
-  serviceentries.networking.istio.io \
-  envoyfilters.networking.istio.io \
-  sidecars.networking.istio.io \
-  peerauthenticaitons.security.istio.io \
-  requestauthentications.security.istio.io \
-  authorizationpolicies.security.isio.io \
-  --timeout=120s
 
-echo "Istio CRDs installed successfully."
+echo "Istio CRDs Installation Finished."
