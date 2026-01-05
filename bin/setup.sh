43333b0ce790f1d2a9014855c58d225534062675
Rurutia1027 <rurutia1027@gmail.com>
2026-01-05 16:45:25 +0800

    refine setup shell scripts rm loki, jeager metricing and tracing components which has no strong relationship with canary

--- bin/setup.sh
+++ bin/setup.sh
@@ -3,16 +3,15 @@ set -euo pipefail
 
 DIR="$(cd "$(dirname "$0")" && pwd)"
 
-bash "$DIR/01-kind-cluster.sh"
+bash "$DIR/01-k8s-cluster.sh"
 bash "$DIR/02-namespaces.sh"
 bash "$DIR/03-helm-repos.sh"
 
-bash "$DIR/10-observability.sh"
-bash "$DIR/20-istio.sh"
-bash "$DIR/30-tracing.sh"
-bash "$DIR/40-kiali.sh"
-
-bash "$DIR/90-rancher.sh"
+bash "$DIR/04-observability.sh"
+bash "$DIR/05-istio.sh"
+# bash "$DIR/06-tracing.sh"
+bash "$DIR/07-kiali.sh"
+# bash "$DIR/08-rancher.sh"
 
 kubectl wait --for=condition=Ready pods --all -n istio-system --timeout=300s
 kubectl wait --for=condition=Ready pods --all -n cattle-system --timeout=300s
